6a81cb1039d94c8cccbea9d251f2e71ae5e8bfc8
void-spark <81029971+void-spark@users.noreply.github.com>
2018-04-03 20:22:53 +0200

    Add mqtt sample code

--- main/main.cpp
+++ main/main.cpp
@@ -10,14 +10,12 @@
 #include "esp_spi_flash.h"
 #include "nvs_flash.h"
 #include "driver/gpio.h"
-#include "esp32_digital_led_lib.h"
-
-
-
 #include "lwip/err.h"
 #include "lwip/sys.h"
-
-
+#include "esp32_digital_led_lib.h"
+extern "C" {
+#include "mqtt_client.h"
+}
 #include "LedBlink2.h"
 #include "RcReceiver.h"
 
@@ -37,6 +35,10 @@
 // WiFi credentials.
 #define WIFI_SSID "Milkrun"
 #define WIFI_PASS "55382636751623425906"
+
+// MQTT details
+const char* mqtt_server = "mqtt://raspberrypi.fritz.box";
+
 
 /* FreeRTOS event group to signal when we are connected*/
 static EventGroupHandle_t wifi_event_group;
@@ -96,9 +98,50 @@ static esp_err_t event_handler(void *ctx, system_event_t *event)
     return ESP_OK;
 }
 
-
-void wifi_init_sta()
+static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
 {
+    esp_mqtt_client_handle_t client = event->client;
+    int msg_id;
+    switch (event->event_id) {
+        case MQTT_EVENT_CONNECTED:
+            ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+            msg_id = esp_mqtt_client_subscribe(client, "/topic/qos0", 0);
+            ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+
+            msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
+            ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+
+            msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos1");
+            ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
+            break;
+        case MQTT_EVENT_DISCONNECTED:
+            ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+            break;
+
+        case MQTT_EVENT_SUBSCRIBED:
+            ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+            msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
+            ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+            break;
+        case MQTT_EVENT_UNSUBSCRIBED:
+            ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+            break;
+        case MQTT_EVENT_PUBLISHED:
+            ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+            break;
+        case MQTT_EVENT_DATA:
+            ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+            printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+            printf("DATA=%.*s\r\n", event->data_len, event->data);
+            break;
+        case MQTT_EVENT_ERROR:
+            ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+            break;
+    }
+    return ESP_OK;
+}
+
+void wifi_init_sta() {
     wifi_event_group = xEventGroupCreate();
 
     tcpip_adapter_init();
@@ -119,6 +162,17 @@ void wifi_init_sta()
     ESP_LOGI(TAG, "wifi_init_sta finished.");
     ESP_LOGI(TAG, "connect to ap SSID:%s password:%s", WIFI_SSID, WIFI_PASS);
 }
+
+
+static void mqtt_app_start(void) {
+    esp_mqtt_client_config_t mqtt_cfg = {};
+    strcpy(mqtt_cfg.uri, mqtt_server);
+    mqtt_cfg.event_handle = mqtt_event_handler;
+
+    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+    esp_mqtt_client_start(client);
+}
+
 
 extern "C" void app_main() {
 
@@ -171,6 +225,8 @@ extern "C" void app_main() {
     xEventGroupWaitBits(wifi_event_group, WIFI_CONNECTED_BIT, false, true, portMAX_DELAY);
 
     blinkerInt.setPattern(ledPatternConnected);
+
+    mqtt_app_start();
 
 
     gpio_config_t io_conf;
