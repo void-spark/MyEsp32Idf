40643a4158bf397342620c3e42578ed3fb30a28d
void-spark <81029971+void-spark@users.noreply.github.com>
2018-04-16 19:36:42 +0200

    Add SD card mp3 playing, which breaks rc interrupt..

--- main/player.cpp
+++ main/player.cpp
@@ -1,9 +1,16 @@
 #include "player.h"
 
+#include <stdio.h>
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/ringbuf.h"
 
 #include "driver/i2s.h"
+
+#include "esp_vfs_fat.h"
+#include "driver/sdspi_host.h"
+#include "sdmmc_cmd.h"
+#include "esp_vfs.h"
 
 #include "i2sstuff.h"
 #include "sound.h"
@@ -18,6 +25,12 @@
 #include <synth.h>
 
 
+#define PIN_NUM_MISO GPIO_NUM_19
+#define PIN_NUM_MOSI GPIO_NUM_5
+#define PIN_NUM_CLK  GPIO_NUM_18
+#define PIN_NUM_CS GPIO_NUM_4
+
+
 #define BCK 23
 #define DIN 22
 #define LCK 21
@@ -27,6 +40,20 @@
 #define LED3_EXT GPIO_NUM_25
 
 static RingbufHandle_t ringBuf = NULL;
+
+#define SDSPI_HOST_DEFAULT_FIXED() {\
+    .flags = SDMMC_HOST_FLAG_SPI, \
+    .slot = HSPI_HOST, \
+    .max_freq_khz = SDMMC_FREQ_DEFAULT, \
+    .io_voltage = 3.3f, \
+    .init = &sdspi_host_init, \
+    .set_bus_width = NULL, \
+    .get_bus_width = NULL, \
+    .set_card_clk = &sdspi_host_set_card_clk, \
+    .do_transaction = &sdspi_host_do_transaction, \
+    .deinit = &sdspi_host_deinit, \
+    .command_timeout_ms = 0, \
+}
 
 void playerSetup() {
     //for 36Khz sample rates, we create 100Hz sine wave, every cycle need 36000/100 = 360 samples (4-bytes or 8-bytes each sample)
@@ -65,7 +92,7 @@ void playerStart() {
     ringBuf = xRingbufferCreate(12*1024, RINGBUF_TYPE_BYTEBUF); // was 16*
 
     printf("Create NET task\n");
-    if (xTaskCreatePinnedToCore(tsknet, "tsknet", 2000, NULL, PRIO_NET, NULL, 1)!=pdPASS) {
+    if (xTaskCreatePinnedToCore(tsknet, "tsknet", 1024*3, NULL, PRIO_NET, NULL, 1)!=pdPASS) {
         printf("ERROR creating NET task! Out of memory?\n");
     };
     printf("Player started\n");
@@ -90,6 +117,92 @@ static mp3player_handle_t mp3Player = NULL;
                 // Good to measure voltage my old tomato :)
 
 static void tsknet(void *pvParameters) {
+
+
+    sdmmc_host_t host = SDSPI_HOST_DEFAULT_FIXED();
+    sdspi_slot_config_t slot_config = SDSPI_SLOT_CONFIG_DEFAULT();
+    slot_config.gpio_miso = PIN_NUM_MISO;
+    slot_config.gpio_mosi = PIN_NUM_MOSI;
+    slot_config.gpio_sck  = PIN_NUM_CLK;
+    slot_config.gpio_cs = PIN_NUM_CS;
+
+    esp_vfs_fat_sdmmc_mount_config_t mount_config = {
+        .format_if_mount_failed = false,
+        .max_files = 5,
+        .allocation_unit_size = 16 * 1024
+    };
+
+    sdmmc_card_t* card = 0;
+    esp_err_t ret = esp_vfs_fat_sdmmc_mount("/sdcard", &host, &slot_config, &mount_config, &card);
+    if (ret != ESP_OK) {
+        if (ret == ESP_FAIL) {
+            printf("Failed to mount filesystem. "
+                "If you want the card to be formatted, set format_if_mount_failed = true.\n");
+        } else {
+            printf("Failed to initialize the card (%s). "
+                "Make sure SD card lines have pull-up resistors in place.\n", esp_err_to_name(ret));
+        }
+        vTaskDelete(NULL);
+    }
+
+    // Card has been initialized, print its properties
+    sdmmc_card_print_info(stdout, card);
+
+    DIR* dir = opendir("/sdcard/MUSIC");
+    if(dir == NULL) {
+        printf("Failed to open dir: %s\n", strerror(errno));
+        vTaskDelete(NULL);
+    }
+    while (true) {
+        struct dirent* de = readdir(dir);
+        if (!de) {
+            break;
+        }
+        printf("File: %s\n", de->d_name);
+    }
+    if(closedir(dir) < 0) {
+        printf("Failed to close dir: %s\n", strerror(errno));
+        vTaskDelete(NULL);
+    }        
+
+    printf("Opening mp3 file\n");
+    FILE* f = fopen("/sdcard/MUSIC/ENGLIS~1.MP3", "rb");
+    if (f == NULL) {
+        printf("Failed to open file for reading: %s\n", strerror(errno));
+        vTaskDelete(NULL);
+    }
+
+    printf("Creating mp3 player\n");
+    mp3Player = mp3player_create(PRIO_MAD, ringBuf);
+    const size_t bufsize2 = 256;
+    uint8_t readBuf2[bufsize2] = {};
+
+    printf("Reading file\n");
+    size_t filesize = 0;
+    while(true) {
+        size_t bytesRead2 = fread(readBuf2, 1, bufsize2, f);
+        if(bytesRead2 == 0) {
+            if(ferror(f)) {
+                printf("Failed to read file: %s\n", strerror(errno));
+                vTaskDelete(NULL);
+            }
+            break;
+        }
+        filesize += bytesRead2;
+        xRingbufferSend(ringBuf, readBuf2, bytesRead2, portMAX_DELAY);
+    }
+    printf("Closing mp3 file, read: %d\n", filesize);
+    fclose(f);
+
+    printf("Destroying mp3 player\n");
+    mp3player_destroy(mp3Player);
+    
+
+    esp_err_t ret2 = esp_vfs_fat_sdmmc_unmount();
+    if (ret2 != ESP_OK) {
+        printf("Failed to unmount the card (%s).\n", esp_err_to_name(ret2));
+        vTaskDelete(NULL);
+    }
 
     int sockfd = socket(AF_INET , SOCK_STREAM, 0);
     if (sockfd < 0) {
