a8536a391364508ac009861f637b6352ef665f4d
void-spark <81029971+void-spark@users.noreply.github.com>
2021-02-13 13:58:04 +0100

    Remove 433 receiver support, us WiFi instead

--- main/main.cpp
+++ main/main.cpp
@@ -15,8 +15,6 @@
 #include "esp32_digital_led_lib.h"
 #include "lwip/apps/sntp.h"
 #include "LedBlink2.h"
-#include "RcReceiver.h"
-#include "doorbell_recv.h"
 #include "sd_doorbell.h"
 #include "my_mqtt.h"
 #include "my_display.h"
@@ -28,11 +26,6 @@
 
 #define WS2812_1 GPIO_NUM_26
 #define NUM_PIXELS 8
-
-#define RCV1_EXT GPIO_NUM_33
-
-#define RC_BITS 25
-#define TRIS (RC_BITS - 1) / 2
 
 // WiFi credentials.
 #define WIFI_SSID "Milkrun"
@@ -58,23 +51,10 @@ LedBlink2 blinkerExt1(LED1_EXT);
 LedBlink2 blinkerExt2(LED2_EXT);
 
 
-RcReceiver rcReceiver(RC_BITS, 12);
-
 strand_t strands[] = { {.rmtChannel = 0, .gpioNum = WS2812_1, .ledType = LED_WS2812B_V3, .numPixels =  NUM_PIXELS,
    .pixels = nullptr, ._stateVars = nullptr} };
 
 strand_t * strand = &strands[0];
-
-void printAc();
-void printDoorbell();
-
-IRAM_ATTR void myHandleInterrupt(void *) {
-  const unsigned long time = esp_timer_get_time();
-  //int state = digitalRead(RCV1_EXT);
-  rcReceiver.handleInterrupt(time);
-  handleDoorbell(time);
-  //digitalWrite(TMP_OUT, state);
-}
 
 static void event_handler(void* arg, esp_event_base_t event_base,
                                 int32_t event_id, void* event_data)
@@ -164,18 +144,7 @@ static void taskRc(void *pvParameters) {
     }
     digitalLeds_updatePixels(strand);
 
-    while (true) {
-        printAc();
-        printf("rc: %d bytes\n", uxTaskGetStackHighWaterMark(NULL));
-    }
-}
-
-static void taskDrb(void *pvParameters) {
-    printf("drb start: %d bytes\n", uxTaskGetStackHighWaterMark(NULL));
-    while (true) {
-        printDoorbell();
-        printf("drb: %d bytes\n", uxTaskGetStackHighWaterMark(NULL));
-    }
+    vTaskDelete(NULL);
 }
 
 static TimerHandle_t buttonTimer;
@@ -194,6 +163,24 @@ static void buttonTimerCallback(TimerHandle_t xTimer) {
 static void subscribeTopics() {
     // TODO: Send node/properties details, rename to setup or so?
     subscribeDevTopic("display/header/set");
+
+    subscribeTopic("devices/receiver/doorbell/pushed");
+}
+
+static bool handleAnyMessage(const char* topic, const char* data) {
+
+    if(strcmp(topic,"devices/receiver/doorbell/pushed") == 0) {
+        blinkerExt2.setPattern(ledPatternDrb, 10);
+
+        //startMarch(13);
+        // startSimple(13);
+
+        sdDoorbellGo();
+
+        return true;
+    }
+
+    return false;
 }
 
 static void handleMessage(const char* topic1, const char* topic2, const char* topic3, const char* data) {
@@ -251,7 +238,7 @@ extern "C" void app_main() {
     sntp_setservername(0, "pool.ntp.org");
     sntp_init();
 
-    mqttStart(subscribeTopics, handleMessage);
+    mqttStart(subscribeTopics, handleMessage, handleAnyMessage);
 
     ESP_LOGI(TAG, "Waiting for MQTT");
     updateHeader("Wait: MQTT");
@@ -260,30 +247,13 @@ extern "C" void app_main() {
 
     blinkerInt.setPattern(ledPatternConnected);
 
-    setupDoorbell();
-
-    gpio_config_t io_conf;
-    io_conf.pin_bit_mask = 1ULL << RCV1_EXT;
-    io_conf.mode = GPIO_MODE_INPUT;
-    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
-    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
-    io_conf.intr_type = GPIO_INTR_ANYEDGE;
-    gpio_config(&io_conf);
-
-    gpio_install_isr_service(0);
-    gpio_isr_handler_add(RCV1_EXT, myHandleInterrupt, NULL);
-
     sdDoorbellStart();
 
     // TimerHandle_t timer = xTimerCreate("infoTimer", pdMS_TO_TICKS(2000), pdTRUE, NULL, infoCallBack );
     // xTimerStart(timer, 0);
-    
-    if (xTaskCreatePinnedToCore(taskRc, "taskRc", configMINIMAL_STACK_SIZE + 2000, NULL, configMAX_PRIORITIES - 5, NULL, 1)!=pdPASS) {
-        printf("ERROR creating taskRc! Out of memory?\n");
-    };
 
-    if (xTaskCreatePinnedToCore(taskDrb, "taskDrb", configMINIMAL_STACK_SIZE + 2000, NULL, configMAX_PRIORITIES - 5, NULL, 1)!=pdPASS) {
-        printf("ERROR creating taskDrb! Out of memory?\n");
+   if (xTaskCreatePinnedToCore(taskRc, "taskRc", configMINIMAL_STACK_SIZE + 2000, NULL, configMAX_PRIORITIES - 5, NULL, 1)!=pdPASS) {
+        printf("ERROR creating taskRc! Out of memory?\n");
     };
 
     gpio_pad_select_gpio(BTN_BOOT);
@@ -293,152 +263,4 @@ extern "C" void app_main() {
     xTimerStart(buttonTimer, 0);
 
     vTaskDelete(NULL);
-}
-
-
-//TODO: 'Move' this to ac_recv, tweak timings for more reliability??, print timings..,
-//check at least 2/ like doorbell. (+ timeout/last????) Reset last on..??? change MAX/CODE_BIT_PULSES?
-// USE ESP TIMERS??
-// Check in/after  hasAcReceived, if it's all valid tri's!! And if address bits are correct?!
-//   AGoal: no weird ones between two identical, which would prevent compare.
-//    Use the longer buffer we have now ????
-// boolean isBit/isSyncBit (isTri??) < array with 'pattern' of this?!
-// Document: shouting on my 'superhetrodyne' cause high pulses to be longer.
-// Could the signal getting to the pin be too low, or otherwise bad?
-// short pulses do seem one of the issues
-
-bool isCorrectCode(uint8_t* result) {
-  return result[0] == TRI_0 && // A0 = GND
-         ( result[1] == TRI_0 || result[1] == TRI_1 ) && // A1 = pin1 (1=UP)
-         ( result[2] == TRI_0 || result[2] == TRI_1 ) && // A2 = pin2 (1=UP)
-         ( result[3] == TRI_0 || result[3] == TRI_1 ) && // A3 = pin3 (1=UP)
-         result[4] == TRI_0 && // A4 = GND
-         result[5] == TRI_0 && // A5 = GND
-         result[6] == TRI_0 && // A6 = GND
-         result[7] == TRI_0 && // A7 = GND
-         ( result[8] == TRI_0 || result[8] == TRI_1 ) && // D0 = switch
-         result[9] == TRI_0 && // D1 = GND
-         result[10] == TRI_0 && // D2 = GND
-         result[11] == TRI_0 ;// D3 = GND
-}
-
-void printAc() {
-
-    static uint8_t triggered[TRIS] = {};
-    static uint8_t last[TRIS] = {};
-
-    struct BitData *bitData = rcReceiver.getReceivedBits();
-    uint8_t *result = rcReceiver.getTris(bitData);
-
-    bool correctCode = isCorrectCode(result);
-
-    rcReceiver.printDetails(bitData);
-    if (!correctCode) {
-        rcReceiver.printAcTimings(bitData);
-    }
-    rcReceiver.printAcTriState(result);
-    if (!correctCode) {
-        return;
-    }
-
-    bool sameAsTriggered = true;
-    bool sameAsLast = true;
-    for (int pos = 0; pos < TRIS; pos++) {
-        if (result[pos] != last[pos]) {
-            sameAsLast = false;
-        }
-        if (result[pos] != triggered[pos]) {
-            sameAsTriggered = false;
-        }
-        last[pos] = result[pos];
-    }
-    if (sameAsLast) {
-        bool stateOn = result[8] == TRI_1;
-
-        uint8_t address = 0;
-        address += result[1] == TRI_1 ? 0 : 1;
-        address += result[2] == TRI_1 ? 0 : 2;
-        address += result[3] == TRI_1 ? 0 : 4;
-
-        static char topic2[50];
-        static char msg2[50];
-        
-        snprintf (topic2, 50, "devices/receiver/sw%d/pulse_%s", address + 1, stateOn ? "on" : "off" );
-        snprintf (msg2, 50, "%s", "true" );
-        mqttPublish(topic2, msg2, 0, 0, 0);
-    }
-
-    if (sameAsLast && !sameAsTriggered) {
-        for (int pos = 0; pos < TRIS; pos++) {
-            triggered[pos] = result[pos];
-        }
-
-        bool stateOn = result[8] == TRI_1;
-
-        uint8_t address = 0;
-        address += result[1] == TRI_1 ? 0 : 1;
-        address += result[2] == TRI_1 ? 0 : 2;
-        address += result[3] == TRI_1 ? 0 : 4;
-
-        blinkerExt1.setPattern(ledPatternRc, address + 1);
-
-
-        printf("Switch %d: %s\n", address + 1, stateOn ? "ON" : "OFF");
-
-        if(stateOn) {
-            // if(address == 0) {
-            //     go();
-            // }
-            strand->pixels[address] = pixelFromRGB(0, 0, 40);
-        } else {
-            strand->pixels[address] = pixelFromRGB(0, 0, 0);
-        }
-        digitalLeds_updatePixels(strand);
-
-
-        static char topic[50];
-        static char msg[50];
-        snprintf (topic, 50, "devices/receiver/sw%d/on", address + 1);
-        snprintf (msg, 50, "%s", stateOn ? "true" : "false"  );
-        mqttPublish(topic, msg, 0, 0, 0);
-    }
-}
-
-
-void printDoorbell() {
-  static uint8_t triggered[DRB_SEQ_LENGTH] = {};
-  static uint8_t last[DRB_SEQ_LENGTH] = {};
-
-  uint8_t* result = getDrbReceivedPulses();
-
-  bool sameAsTriggered = true;
-  bool sameAsLast = true;
-  for (int pos = 0; pos < DRB_SEQ_LENGTH; pos++) {
-    if (result[pos] != last[pos]) {
-      sameAsLast = false;
-    }
-    if (result[pos] != triggered[pos]) {
-      sameAsTriggered = false;
-    }
-    last[pos] = result[pos];
-  }
-  if (sameAsLast && !sameAsTriggered ) {
-    for (int pos = 0; pos < DRB_SEQ_LENGTH; pos++) {
-      triggered[pos] = result[pos];
-    }
-    blinkerExt2.setPattern(ledPatternDrb, 10);
-    printDoorbellWord(result, DRB_SEQ_LENGTH);
-
-    //startMarch(13);
-    // startSimple(13);
-
-    static char topic[50];
-    static char msg[50];
-    snprintf (topic, 50, "devices/receiver/doorbell/pushed");
-    snprintf (msg, 50, "%s", "true" );
-
-    mqttPublish(topic, msg, 0, 0, 0);
-
-    sdDoorbellGo();
-  }
 }
