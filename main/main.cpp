334b37da6e601fe9a071f14cacbb72145888cb1a
void-spark <81029971+void-spark@users.noreply.github.com>
2021-07-09 22:42:12 +0200

    Use wifi_helper and mqtt_helper

--- main/main.cpp
+++ main/main.cpp
@@ -15,8 +15,9 @@
 #include "esp32_digital_led_lib.h"
 #include "lwip/apps/sntp.h"
 #include "LedBlink2.h"
+#include "wifi_helper.h"
+#include "mqtt_helper.h"
 #include "sd_doorbell.h"
-#include "my_mqtt.h"
 #include "my_display.h"
 
 #define BTN_BOOT GPIO_NUM_0
@@ -30,11 +31,6 @@
 // WiFi credentials.
 #define WIFI_SSID "Milkrun"
 #define WIFI_PASS "55382636751623425906"
-
-/* FreeRTOS event group to signal app status changes*/
-static EventGroupHandle_t app_event_group;
-
-const int WIFI_CONNECTED_BIT = BIT0;
 
 static const char *TAG = "MyEsp32";
 
@@ -55,66 +51,6 @@ strand_t strands[] = { {.rmtChannel = 0, .gpioNum = WS2812_1, .ledType = LED_WS2
    .pixels = nullptr, ._stateVars = nullptr} };
 
 strand_t * strand = &strands[0];
-
-static void event_handler(void* arg, esp_event_base_t event_base,
-                                int32_t event_id, void* event_data)
-{
-    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
-        esp_wifi_connect();
-    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
-        esp_wifi_connect();
-        ESP_LOGI(TAG, "retry to connect to the AP");
-        xEventGroupClearBits(app_event_group, WIFI_CONNECTED_BIT);
-    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
-        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
-        {
-            char ip[16] = "";
-            snprintf(ip, 16, IPSTR, IP2STR(&event->ip_info.ip));
-            updateIp(ip);
-        }
-        xEventGroupSetBits(app_event_group, WIFI_CONNECTED_BIT);
-    }
-}
-
-void wifi_init_sta() {
-    app_event_group = xEventGroupCreate();
-
-    ESP_ERROR_CHECK(esp_netif_init());
-
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_create_default_wifi_sta();
-
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
-    esp_event_handler_instance_t instance_any_id;
-    esp_event_handler_instance_t instance_got_ip;
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
-                                                        ESP_EVENT_ANY_ID,
-                                                        &event_handler,
-                                                        NULL,
-                                                        &instance_any_id));
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
-                                                        IP_EVENT_STA_GOT_IP,
-                                                        &event_handler,
-                                                        NULL,
-                                                        &instance_got_ip));
-
-    wifi_config_t wifi_config = {};
-    strcpy((char*)wifi_config.sta.ssid, WIFI_SSID);
-    strcpy((char*)wifi_config.sta.password, WIFI_PASS);
-    wifi_config.sta.pmf_cfg.capable = true;
-    wifi_config.sta.pmf_cfg.required = false;
-
-    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
-    ESP_ERROR_CHECK(esp_wifi_start() );
-
-    ESP_LOGI(TAG, "wifi_init_sta finished.");
-    ESP_LOGI(TAG, "connect to ap SSID:%s password:%s", WIFI_SSID, WIFI_PASS);
-}
 
 extern "C" {
     static void infoCallBack(TimerHandle_t xTimer) {
@@ -223,13 +159,17 @@ extern "C" void app_main() {
     myInitDisplay();
 
     // Initialize WiFi
-    wifi_init_sta();
+    wifiStart();
 
     blinkerInt.setPattern(ledPatternPendingWiFi);
     
     ESP_LOGI(TAG, "Waiting for wifi");
     updateHeader("Wait: WiFi");
-    xEventGroupWaitBits(app_event_group, WIFI_CONNECTED_BIT, false, true, portMAX_DELAY);
+    wifiWait();
+
+    tcpip_adapter_ip_info_t ipInfo = {}; 
+    tcpip_adapter_get_ip_info(TCPIP_ADAPTER_IF_STA, &ipInfo);
+    updateIp(ip4addr_ntoa(&ipInfo.ip));
 
     blinkerInt.setPattern(ledPatternPendingMqtt);
 
